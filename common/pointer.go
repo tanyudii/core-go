9401c4267ba851f3c4de5e94d1064bea91159fa7
Yudi Hertanto <y.hertanto17@gmail.com>
2023-02-16 22:41:27 +0700

    [feat] safety pointer val

--- common/pointer.go
+++ common/pointer.go
@@ -1,5 +1,23 @@
 package common
 
+import "reflect"
+
 func PointerVal[T any](val T) *T {
 	return &val
+}
+
+func SafetyEmptyAsNil[T any](val T) *T {
+	var zero T
+	if reflect.ValueOf(&val).Elem().IsZero() {
+		return nil
+	}
+	return &zero
+}
+
+func SafetyPointerVal[T any](val *T) T {
+	var result T
+	if val == nil {
+		return result
+	}
+	return *val
 }
