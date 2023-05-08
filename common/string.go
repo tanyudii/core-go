550b8c4989917a239d45c7eacb75cdb7856af520
Yudi Hertanto <y.hertanto17@gmail.com>
2023-05-08 22:34:06 +0700

    [feat] add utilities string

--- common/string.go
+++ common/string.go
@@ -18,3 +18,25 @@ func RFC5322Format(name, email string) string {
 	}
 	return fmt.Sprintf("%s <%s>", name, email)
 }
+
+func SeparatedStringToMapBool(val, sep string) map[string]bool {
+	if val == "" {
+		return nil
+	}
+	ret := make(map[string]bool)
+	for _, v := range strings.Split(val, sep) {
+		ret[v] = true
+	}
+	return ret
+}
+
+func AppendSeparatedString(current, new, sep string) string {
+	if current == "" {
+		return new
+	}
+	return current + sep + new
+}
+
+func IsStringInSeparatedString(val, check, sep string) bool {
+	return SeparatedStringToMapBool(val, sep)[check]
+}
